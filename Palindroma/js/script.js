3a51d594835e61b06dfdddf619e25b283baa8826
marianitadileo <mdileo36@gmail.com>
2023-03-22 12:53:15 +0100

    add exercise palindroma

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,4 +1,34 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma.
 
-const word = prompt("Inserisci il tuo nome");
+let word = prompt("Inserisci una parola");
+let result = numeroPalindromo (word);
+console.log (result);
+
+function numeroPalindromo (word) {
+
+    let palindrome = false;
+    let i = 0;
+    let less = word.length -1;
+
+    while (i <= word.length) {
+
+        if (i > less) {
+            palindrome = true;
+            i = word.length + 1;
+
+        } else if ( i < word.length && word[i] == word[less]) {
+            i++;
+            less--;
+        } else {
+            i = word.length + 1
+        }
+    }
+
+        if (palindrome == true) {
+            console.log ('la parola è palindroma!');
+        } else {
+            console.log ('la parola non è palindroma');
+        }
+        return palindrome;
+}
